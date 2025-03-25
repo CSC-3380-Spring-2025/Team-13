fc8cc8b5129d585d3e346d844d719719c4481802
nfall3 <nfall3@lsu.edu>
2025-03-24 20:45:11 -0400

    Update db.ts

--- my-app/backend/database/db.ts
+++ my-app/backend/database/db.ts
@@ -3,59 +3,55 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+
 const pool = mysql.createPool({
-
-host: process.env.DATABASE_HOST,
-user: process.env.DATABASE_USER,
-password: process.env.DATABASE_PASSWORD,
-database: process.env.DATABASE_NAME,
-port: Number(process.env.DATABASE_PORT) || 3306, //Default for MySQL
-
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  port: Number(process.env.DATABASE_PORT) || 3306, // Default MySQL 
   waitForConnections: true,
-  connectionLimit: 5,
-  queueLimit: 1,
+  connectionLimit: 10, 
+  queueLimit: 5,
 });
 
-async function testConnection(){
-  try{
+
+async function testConnection() {
+  try {
     const connection = await pool.getConnection();
-  
-if (connection){
-  console.log("Successfully connected to MySQL Database");
-  connection.release();
-}else{
-  console.log("Was not able to connect to MySQL");
-
-}
-  }catch (error){
+    console.log("Successfully connected to MySQL Database");
+    connection.release();
+  } catch (error) {
     console.error("Unable to connect to MySQL:", (error as Error).message);
-
   }
 }
 
-async function checkForUser(email: string){
-  try{
+
+async function checkForUser(email: string) {
+  try {
     const connection = await pool.getConnection();
 
-    const query = "Select username, email and password from the users email = ?";
-    const [rows] = await connection.execute( query, [email]);
+
+    const query = "SELECT username, email, password FROM users WHERE email = ?";
+    const [rows] = await connection.execute(query, [email]);
+    
     connection.release();
 
-
-    if((rows as any [].length > 0){
-      console.log("Successfully found user.", rows);
+    if ((rows as any[]).length > 0) {
+      console.log("Successfully found user:", rows[0]);
       return rows[0];
-
-    }else{
-      console.log("User not found, try a different email.");
+    } else {
+      console.log("User not found. Try a different email.");
       return null;
     }
-  }catch (error){
-    console.error("Error in checking for user information", (error as Error).message);
+  } catch (error) {
+    console.error("Error checking for user information:", (error as Error).message);
     return null;
   }
 }
-testConnection();
-export { pool, checkUser };
 
+
+testConnection();
+
+export { pool, checkForUser };
 
