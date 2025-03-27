774b3e1e97149d4a02b78d4421888b78045e8be4
nfall3 <nfall3@lsu.edu>
2025-03-27 16:54:00 -0400

    Update db.ts

--- my-app/backend/database/db.ts
+++ my-app/backend/database/db.ts
@@ -1,2 +1,60 @@
+const mysql = require ("mysql2");
+const dotenv = require ("dotenv");
+
+dotenv.config();
+const connection = mysql.createConnection({
+  host: process.env.DATABASE_HOST || "localhost",
+  user: process.env.DATABASE_USER || "root",
+  password: process.env.DATABASE_PASSWORD || "", // Password not public
+  database: process.env.DATABASE_NAME || "node_connect",
+  port: Number(process.env.DATABASE_PORT) || 3306, // Default MySQL 
+  
+});
+
+interface IUserRow{
+  id_number: number;
+  username: string;
+  email: string;
+  created_at: string;
+}
+const userEmail = "user@testexample.com";
+
+
+connection.connect((error) => {
+  if(error){
+    console.log("Unable to connect to MySQL", error);
+    return;
+  }
+  
+  console.log("Successfully connected to MySQL Database");
+  const sql = "SELECT * FROM users WHERE email = ?";
+  
+  connection.query(sql,  [userEmail], (error: any, results: IUserRow[]) =>{
+    if (error){
+    console.log("Unable to connect to the query", error);
+    return;
+}
+if (results.length > 0){
+  console.log("Successfully found user");
+  console.log(results[0]);
+} else {
+  console.log("Unable to find user's email");
+}
+
+  
+connection.end((error) => {
+   if(error){
+    console.log("Unable to close connection", error);
+    return;
+  
+}
+  console.log("Connection closed");
+      });
+   });
+ });
+
+
+    
+
 
 
