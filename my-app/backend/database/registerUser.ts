b3e9b208bc5bdf88152e57c5298fb18890693821
nfall3 <nfall3@lsu.edu>
2025-04-07 14:33:43 -0500

    Update registerUser.ts

--- my-app/backend/database/registerUser.ts
+++ my-app/backend/database/registerUser.ts
@@ -1,52 +1,52 @@
-const mysql = require ("mysql2");
-const dotenv = require ("dotenv");
-const readline = require ("readline");
+const mysql = require("mysql2");
+const dotenv = require("dotenv");
+const readline = require("readline");
 
 dotenv.config();
+
 const rl = readline.createInterface({
   input: process.stdin,
-    output: process.stdout,
-  });
-
-rl. question(" Enter your username: ", (username: string) => {
-  rl.question("Enter your email: ", (email: string) => {
-
-  
-const connection = mysql.createConnection({
-  host: process.env.DATABASE_HOST || "localhost",
-  user: process.env.DATABASE_USER || "root",
-  password: process.env.DATABASE_PASSWORD || "", // Password not public
-  database: process.env.DATABASE_NAME || "node_connect",
-  port: Number(process.env.DATABASE_PORT) || 3306, // Default MySQL 
-  
+  output: process.stdout,
 });
 
-connection.connect((error) => {
-  if(error){
-    console.log("Unable to connect to MySQL", error);
-    return;
-  }
-  
-console.log("Successfully connected to MySQL Database");
-  const sql = "INSERT INTO users (username, email) VALUES (?,?)";
-  const values = [username, email};
+rl.question("👤 Enter your username: ", (username: string) => {
+  rl.question("📧 Enter your email: ", (email: string) => {
+    const connection = mysql.createConnection({
+      host: process.env.DATABASE_HOST || "localhost",
+      user: process.env.DATABASE_USER || "root",
+      password: process.env.DATABASE_PASSWORD || "",
+      database: process.env.DATABASE_NAME || "node_connect",
+      port: Number(process.env.DATABASE_PORT) || 3306, // Default in MySQL
+    });
 
-connection.query(sql,  values, (error: any, result: any) =>{
-    if (error){
-    console.log("Error inserting new user", error.message);
-} else{
-      console.log("Successfully registered user");
+    connection.connect((error: any) => {
+      if (error) {
+        console.log("Unable to connect to MySQL database", error);
+        return;
+      }
 
-   }
+      console.log(" Successfully connected to MySQL Database");
 
-  connection.end((error) => {
-   if(error){
-    console.log("Unable to close connection", error.message);
-    return;
-  
-}
-  console.log("Connection closed");
+      const sql = "INSERT INTO users (username, email) VALUES (?, ?)";
+      const values = [username, email];
+
+      connection.query(sql, values, (error: any, result: any) => {
+        if (error) {
+          console.log("Error inserting new user:", error.message);
+        } else {
+          console.log("Successfully registered user");
+        }
+
+        connection.end((error: any) => {
+          if (error) {
+            console.log("Error, connection not closed:", error.message);
+            return;
+          }
+          console.log(" Connection successfully closed");
+          rl.close(); 
+        });
+      });
     });
   });
 });
-  
+
