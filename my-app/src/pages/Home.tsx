f327267958487e8d50bbcd274770bf18a378e7e3
tylerlallande <tlalla1@lsu.edu>
2025-04-16 10:14:48 -0500

    Navbar & login page

--- my-app/src/pages/Home.tsx
+++ my-app/src/pages/Home.tsx
@@ -3,7 +3,67 @@ import '../styles/Home.css'
 
 const Home = () => {
   return (
-    <div className='home-container'></div>
+    <div className='home-container'>
+      <header className = "header"> 
+        <a href="#" className ="audio">Impressions Audio</a>
+   
+        <nav className="navbar">
+                <a href="home.html">Home</a>
+                <a href="library.html">Library</a>
+                <a href="discover.html">Discover</a>
+                <a href="about.html">About</a>
+        </nav>
+    </header>
+
+
+    <h1> This is the discover Page</h1>
+   
+    <div className="photos">
+        <a href="https://youtu.be/-mNEr0OGusU?si=1z9wYC1lB77gb1Vf"> <img src="mother.jpg"  width="300" height="300"/> </a>
+     </div>
+     
+     <div className="photos"><a> <img src="pink friday.jpg" height="300" width="300"/> </a> 
+     </div>
+     
+     <div className="photos"><a> <img src="Eternal-Sunshine-Cover.jpg" height="300" width="300"/> </a> 
+     </div>
+  
+     <div className="photos"><a> <img src="croc bites.jpg" height="300" width="300"/> </a> 
+     </div>
+  
+     <div className="photos"><a> <img src="anxiety.jpg" height="300" width="300"/> </a> 
+     </div>
+  
+     <div className="photos"><a> <img src="ehaven or hell.jpg" height="300" width="300"/> </a> 
+     </div>
+     
+     <div className="photos"><a> <img src="beyonce.png" height="300" width="300"/> </a> 
+     </div>
+  
+     <div className="photos"><a> <img src="romance.jpg" height="300" width="300"/> </a> 
+     </div>
+     
+     <div className="photos"><a> <img src="croc bites.jpg" height="300" width="300"/> </a> 
+     </div>
+     
+     <div className="photos"><a> <img src="croc bites.jpg" height="300" width="300"/> </a> 
+     </div
+     
+     ><div className="photos"><a> <img src="croc bites.jpg" height="300" width="300"/> </a> 
+     </div>
+     
+     <div className="photos"><a> <img src="croc bites.jpg" height="300" width="300"/> </a> 
+     </div>
+     
+     <div className="photos"><a> <img src="croc bites.jpg" height="300" width="300"/> </a> 
+     </div>
+     
+     <div className="photos"><a> <img src="croc bites.jpg" height="300" width="300"/> </a> 
+     </div>
+     
+     <div className="photos"><a> <img src="croc bites.jpg" height="300" width="300"/> </a> 
+     </div>
+    </div>
 
   );
 }
