f7612fb12f2e59aba81f82c4d3092ddaf693acbb
Nene Fall <nenefall@Nenes-Laptop.local>
2025-04-27 13:07:11 -0500

    Add complete Signup system with frontend (client) and backend (server)

--- my-app/src/components/Navbar.tsx
+++ my-app/src/components/Navbar.tsx
@@ -1,14 +1,21 @@
+import React from 'react';
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css"
 
-export default function Navbar() {
+const Navbar: React.FC = () => {
+
   return (
-    <div>
+    <nav>
+       <ul>
       <li><Link to="/">Home</Link></li>
       <li><Link to="/search">Search</Link></li>
       <li><Link to="/playlists">Playlists</Link></li>
       <li><Link to="/survey">Survey Page</Link></li>
       <li><Link to="/concerts">Concerts</Link></li>
-    </div>
-  )
-}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
+
---
+++ server/backend/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI # type: ignore
+import spotipy # type: ignore
+from spotipy.oauth2 import SpotifyClientCredentials # type: ignore
+import requests # type: ignore
+import os
+
+app = FastAPI()
+
+CLIENT_ID = "d2f24b7ead2a43ab9847c27be93bcdc4"
+CLIENT_SECRET = "1e781ba379e345a6bd2a94fa7ca8b998"
+
+spotify = spotipy.Spotify(auth_manager=SpotifyClientCredentials(
+    client_id=CLIENT_ID, 
+    client_secret=CLIENT_SECRET
+    )
+)
+
+@app.get("/")
+def read_root():
+    return {"message": "FastAPI"}
+
+@app.get("/search")
+def search_song(query: str):
+    results = spotify.search(q=query, limit=20, type="tracks")
+    return {"tracks": results["tracks"]["items"]}
